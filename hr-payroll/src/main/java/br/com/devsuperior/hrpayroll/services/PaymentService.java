dcd5cb39d1bd3210a38588d757a62649d5bc9b5e
alyf009 <iranalyf>
2021-09-19 13:28:40 -0300

    impl feign

--- hr-payroll/src/main/java/br/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -3,6 +3,7 @@ package br.com.devsuperior.hrpayroll.services;
 import br.com.devsuperior.hrpayroll.config.AppConfig;
 import br.com.devsuperior.hrpayroll.dto.Worker;
 import br.com.devsuperior.hrpayroll.entities.Payment;
+import br.com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,17 +16,13 @@ import java.util.Map;
 @AllArgsConstructor
 public class PaymentService {
 
-    private RestTemplate restTemplate;
-
     private AppConfig appConfig;
 
-    public Payment getPayment(Long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
+    private WorkerFeignClient workerFeignClient;
 
-        ResponseEntity<Worker> forEntity = restTemplate
-                .getForEntity(appConfig.getWorkerHost() + "/workers/{id}", Worker.class, uriVariables);
-        return Payment.builder().name(forEntity.getBody().getName()).dailyIncome(forEntity.getBody().getDailyIncome())
+    public Payment getPayment(Long workerId, int days){
+        ResponseEntity<Worker> byId = workerFeignClient.findById(workerId);
+        return Payment.builder().name(byId.getBody().getName()).dailyIncome(byId.getBody().getDailyIncome())
                 .days(days).build();
     }
 }
